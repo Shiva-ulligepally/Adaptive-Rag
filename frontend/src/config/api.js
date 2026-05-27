6376d1026aa71d3374f171eed3d8b7efc5709b11
Shiva-ulligepally <u.shiva87906@gmail.com>
2026-05-27 11:52:02 +0530

    Fixed Vercel deployment and production configuration

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,6 +2,7 @@ const API_BASE =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
-    : "/_/backend";
+    : "";
+
 
 export default API_BASE;
